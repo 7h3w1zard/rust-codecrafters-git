8376266441d00a1a72d0fe63935929a3438920cb
7h3w1zard <93vrs93@gmail.com>
2025-11-04 15:10:01 +0300

    Read a blob object #ic4

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use anyhow::Context;
+use flate2::read::ZlibDecoder;
+use std::ffi::CStr;
+use std::fs;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::Write;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+/// Doc comment
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Doc comment
+    Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
+}
+
+enum Kind {
+    Blob,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+        Command::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            anyhow::ensure!(
+                pretty_print,
+                "mode must be given without -p, and we don't support mode"
+            );
+
+            let f = std::fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))
+            .context("open in .git/objects")?;
+            let z = ZlibDecoder::new(f);
+            let mut z = BufReader::new(z);
+            let mut buf = Vec::new();
+            z.read_until(0, &mut buf)
+                .context("read header from .git/objects")?;
+            let header = CStr::from_bytes_with_nul(&buf)
+                .expect("know there is exactly one nul, and it's at the end");
+            let header = header
+                .to_str()
+                .context(".git/objects file header isn't valid UTF-8")?;
+            let Some((kind, size)) = header.split_once(" ") else {
+                anyhow::bail!(
+                    ".git/objects file header did not start with a known type: '{header}'"
+                );
+            };
+            let kind = match kind {
+                "blob" => Kind::Blob,
+                _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
+            };
+            let size = size
+                .parse::<u64>()
+                .context(".git/objects file header has invalid size: {size}")?;
+            let mut z = z.take(size);
+            match kind {
+                Kind::Blob => {
+                    let stdout = std::io::stdout();
+                    let mut stdout = stdout.lock();
+                    let n = std::io::copy(&mut z, &mut stdout)
+                        .context("write .git/objects file to stdout")?;
+                    stdout
+                        .write_all(&buf)
+                        .context("write object contents to stdout")?;
+                    anyhow::ensure!(
+                        n == size,
+                        ".git/objects was not the expected size (expected: {size}, actual: {n})"
+                    );
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
